e7600cfb07a1936b80013bf29d380e1f614332c3
Hemanth <thaluruhemanth105@gmail.com>
2022-03-03 15:33:28 +0530

    Add project files.

---
+++ Retail_Bank_UI/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using AccountMicroservice.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+
+namespace Retail_Bank_UI.Controllers
+{
+  
+    public class AdminController : Controller
+    {
+
+        public async Task<IActionResult> Index()
+        {
+            Client client = new Client();
+            List<Account> accounts = new List<Account>();
+            try {
+                var result = await client.APIClient().GetAsync("/gateway/Account/getAllAccounts");
+                if (result.IsSuccessStatusCode)
+                {
+                    var account = result.Content.ReadAsStringAsync().Result;
+                    accounts = JsonConvert.DeserializeObject<List<Account>>(account);
+                }
+                return View(accounts);
+            }
+            catch(Exception e)
+            {
+                ViewBag.Error = e.Message;
+                return View();
+            }
+            }
+    }
+}
